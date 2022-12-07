6fb8c6f7acb8622b80ebbd3679dca2772d8db1ec
umerkhann <umeriskhan7@gmail.com>
2022-12-08 02:47:58 +0500

    About page images overflow fixed!

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ function App() {
   return (
     <>
     {/* <CategoryCards /> */}
-    {/* <About /> */}
-    <Contact />
+    <About />
+    {/* <Contact /> */}
     <Footer />
     </>
   );
--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,5 +1,4 @@
 import "./About.css";
-import AboutImage from "../Image/bg-01.jpg.webp";
 import AboutImage1 from "../Image/about-01.jpg.webp";
 import AboutImage2 from "../Image/about-02.jpg.webp";
 import { Container, Row, Col } from "react-bootstrap";
@@ -10,8 +9,8 @@ const About = () => {
     <>
       <PageInfo title="About"/>
       <Container>
-        <Row className="p-t-40">
-          <Col md={7} lg={8}>
+        <Row className="p-t-40 d-flex" >
+          <Col md={7} lg={8} sm={12}>
             <Container>
               <h3>Our Story</h3>
               <p style={{ paddingTop: "20px" }}>
@@ -46,21 +45,20 @@ const About = () => {
               </p>
             </Container>
           </Col>
-          <Col md={5} lg={4}>
-            <Container>
-              {" "}
+          <Col md={5} lg={4} sm={12}>
+            <Container className="image-overflow">
               <img className="img-size" src={AboutImage1} alt="IMG" />
-            </Container>{" "}
+            </Container>
           </Col>
         </Row>
-        <Row>
-        <Col md={5} lg={4}>
-            <Container>
-              {" "}
+        <Row className="p-t-40 justify-content-evenly">
+        <Col md={{span: 5, order:1}} lg={{span: 4, order:1}} sm={{span: 12, order:2}}>
+            <Container className="image-overflow">
+              
               <img className="img-size" src={AboutImage2} alt="IMG" />
-            </Container>{" "}
+            </Container>
           </Col>
-          <Col md={7} lg={8}>
+          <Col md={{span: 7, order:2}} lg={{span: 8, order:2}} sm={{span: 12, order:1}}>
             <Container>
               <h3>Our Story</h3>
               <p style={{ paddingTop: "20px" }}>
@@ -78,21 +76,6 @@ const About = () => {
                 facilisis diam faucibus et. Morbi a tempor elit.
               </p>
 
-              <p style={{ paddingTop: "20px" }}>
-                Donec gravida lorem elit, quis condimentum ex semper sit amet.
-                Fusce eget ligula magna. Aliquam aliquam imperdiet sodales. Ut
-                fringilla turpis in vehicula vehicula. Pellentesque congue ac
-                orci ut gravida. Aliquam erat volutpat. Donec iaculis lectus a
-                arcu facilisis, eu sodales lectus sagittis. Etiam pellentesque,
-                magna vel dictum rutrum, neque justo eleifend elit, vel
-                tincidunt erat arcu ut sem. Sed rutrum, turpis ut commodo
-                efficitur, quam velit convallis ipsum, et maximus enim ligula ac
-                ligula.
-              </p>
-              <p style={{ paddingTop: "20px" }}>
-                Any questions? Let us know in store at 8th floor, 379 Hudson St,
-                New York, NY 10018 or call us on (+1) 96 716 6879
-              </p>
             </Container>
           </Col>
         </Row>
